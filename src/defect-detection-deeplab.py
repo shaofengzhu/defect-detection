1251870d7285bb5d5237a65c05a0d862c7a9807d
Shaofeng Zhu <shaozhu@microsoft.com>
2021-08-14 13:16:50 -0700

    Test accuracy

--- src/defect-detection-deeplab.py
+++ src/defect-detection-deeplab.py
@@ -29,7 +29,7 @@ batch_size = 2
 image_width = 716 // 4
 image_height = 920 // 4
 learning_rate = 0.0001
-num_epochs = 4
+num_epochs = 2
 
 class ImageDataset(Dataset):
     def __init__(self, src_image_folder, label_image_folder):
@@ -158,7 +158,7 @@ def load_checkpoint_and_train_and_save_model():
 
     # optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
-    checkpoint = torch.load("saved-deeplab-checkpoint.pth")
+    checkpoint = torch.load(os.path.join(current_location, "saved-deeplab-checkpoint.pth"))
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
     train_loop(train_loader, model, criterion, optimizer)
@@ -181,7 +181,7 @@ def train_loop(train_loader, model, criterion, optimizer):
             loss.backward()
             optimizer.step()
 
-            if batch_index % 20 == 0:
+            if batch_index % 10 == 0:
                 print(f"epoch {epoch}, step {batch_index} / {n_total_steps}, loss={loss}")
 
             # During test, we could break earlier
@@ -265,6 +265,59 @@ def test_model():
 
             index += 1
 
+def test_model_accuracy():
+    test_dataset = ImageDataset(test_src_image_folder, test_label_image_folder)
+    # test_dataset = ImageDataset(train_src_image_folder, train_label_image_folder)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=1, shuffle=False)
+
+    model = torch.load(os.path.join(current_location, "saved-deeplab.pth"))
+    model.eval()
+    criterion = nn.BCEWithLogitsLoss()
+    with torch.no_grad():
+        index = 0
+        target_index = None
+        accuracy_list = []
+
+        for image, label in test_loader:
+            output = model(image)
+            target = label.float()
+            loss = criterion(output, target)
+            print(f"loss={loss}")
+
+            output_raw = output.squeeze(0)
+
+            # the index who has the max value. As we only have two classes, it's either 0 or 1
+            output_label = output_raw.argmax(dim=0)
+
+            image_data = image.squeeze(0)[0]
+            label_data = label.squeeze(0)[1]
+
+            if index == target_index:
+                print(label_data - output_label)
+                print(torch.abs(label_data - output_label).sum())
+
+                # draw the images
+                fig, axarr = plt.subplots(3)
+                # show the image
+                axarr[0].imshow(image_data, cmap = "gray")
+                # expected label
+                axarr[1].imshow(label_data, cmap = "gray")
+                # defect label
+                axarr[2].imshow(output_label, cmap = "gray")
+                plt.show()
+
+            # As we only have 0 and 1, the simple way is to substract two matrix
+            # then get the sum of the absoluate value. Those are the pixels have difference
+            accuracy = 1.0 - torch.abs(label_data - output_label).sum().item() / (image_height * image_width)            
+            accuracy_list.append(accuracy)
+
+            index = index + 1
+
+        accuracy_list_np = np.array(accuracy_list)
+        print(f"Accuracy: min={accuracy_list_np.min()}, max={accuracy_list_np.max()}, average={np.average(accuracy_list_np)}")
+        print(f"The least accuracy index={accuracy_list_np.argmin()}")
+
+
 def test_predict_using_pretrained_standard_model():
     """
     Predict the object using pretrained standard model
@@ -319,6 +372,7 @@ if __name__ == "__main__":
     # test_train_one()
     # train_and_save_model()
     # load_checkpoint_and_train_and_save_model()
-    test_model()
+    # test_model()
+    test_model_accuracy()
     # test_predict_using_pretrained_standard_model()
 
